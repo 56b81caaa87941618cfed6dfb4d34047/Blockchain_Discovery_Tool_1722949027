62679abf5b51c8d66d9dd325f9c84e7acd2b86bb
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-06 12:57:20 +0000

    Placeholder text replacement

--- components/simple_footer_component_1722949029.js
+++ components/simple_footer_component_1722949029.js
@@ -9,13 +9,13 @@ Vue.component("simple_footer_component_1722949029", {
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            STARTER_TEMPLATE_APPLICATION_NAME
+                            BlockScan - Decentralized Data Explorer
                         </a>
                     </div>
                 </div>
                 <div class="flex">
                     <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                        STARTER_TEMPLATE_FOOTER_CONTENT
+                        Stay ahead of the curve with BlockScan. Our cutting-edge platform scours the blockchain for the latest data and information, giving you a competitive edge in the rapidly evolving world of decentralized technology.
                     </div>
                 </div>
             </div>
